ee6d71b06725bbd7a668cb2f056b0ac49c0f1280
xwoud <xwoud@naver.com>
2021-01-20 00:00:22 +0900

    Apply .gitignore

---
+++ About-SwiftUI/About-SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  About-SwiftUI
+//
+//  Created by 김민희 on 2021/01/19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
