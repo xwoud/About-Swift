2b36667838794f28f5818ae2d69b85a03d6dd190
xwoud <xwoud@naver.com>
2021-07-09 16:42:00 +0900

    [upload] 뷰 구성

--- TableWatchApp/TableWatchApp WatchKit Extension/ContentView.swift
+++ TableWatchApp/TableWatchApp WatchKit Extension/ContentView.swift
@@ -7,10 +7,27 @@
 
 import SwiftUI
 
+var listData = [
+    ListData(name: "구글", bgColor: .secondary),
+    ListData(name: "네이버", bgColor: .green),
+    ListData(name: "애플", bgColor: .gray),
+    ListData(name: "다음", bgColor: .purple),
+]
+
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
-            .padding()
+        List {
+            ForEach(listData) { item in
+                HStack {
+                    Text(item.name)
+                    Spacer()
+                }
+                .padding()
+                .background(item.bgColor)
+                .cornerRadius(10)
+            }
+            .listRowBackground(Color.clear)
+        }
     }
 }
 
--- TableWatchApp/TableWatchApp WatchKit Extension/Struct/ListData.swift
+++ TableWatchApp/TableWatchApp WatchKit Extension/Struct/ListData.swift
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-struct listData: Identifiable {
+struct ListData: Identifiable {
     // 데이터를 저장할 구조체 생성
     
     var id = UUID()
