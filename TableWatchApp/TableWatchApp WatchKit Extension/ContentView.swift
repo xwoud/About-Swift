bca60bb3ebe6df0858a2201cecacae4e8e041cf0
xwoud <xwoud@naver.com>
2021-07-09 16:40:07 +0900

    [upload] 데이터를 저장할 구조체 생성

---
+++ TableWatchApp/TableWatchApp WatchKit Extension/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TableWatchApp WatchKit Extension
+//
+//  Created by Hailey on 2021/07/09.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TableWatchApp/TableWatchApp WatchKit Extension/Struct/listData.swift
@@ -0,0 +1,16 @@
+//
+//  listData.swift
+//  TableWatchApp WatchKit Extension
+//
+//  Created by Hailey on 2021/07/09.
+//
+
+import SwiftUI
+
+struct listData: Identifiable {
+    // 데이터를 저장할 구조체 생성
+    
+    var id = UUID()
+    var name: String
+    var bgColor: Color
+}
