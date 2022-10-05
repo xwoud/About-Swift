7ae270c40202749d6155b40589f4a8ae219c3fcd
xwoud <xwoud@naver.com>
2022-10-05 14:36:57 +0900

    [upload] MenuItem프로젝트 생성

---
+++ MenuItemApp/MenuItemApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MenuItemApp
+//
+//  Created by Hailey on 2022/10/05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
